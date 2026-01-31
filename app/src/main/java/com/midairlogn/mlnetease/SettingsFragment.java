5905012dab3151e72b477040ecbea663d5318c97
midairlogn <midairlog@gmail.com>
2026-01-31 09:34:08 +0800

    update LICENSE
    update LICENSE year from 2025 to 2026

--- app/src/main/java/com/midairlogn/mlnetease/SettingsFragment.java
+++ app/src/main/java/com/midairlogn/mlnetease/SettingsFragment.java
@@ -76,7 +76,7 @@ public class SettingsFragment extends Fragment {
             // Use default
         }
         String infoText = "Version: v" + versionName + "   Author: <a href=\"https://github.com/midairlogn\">Midairlogn</a><br>" +
-                "<a href=\"https://github.com/midairlogn/ML-Netease_Android\">ML-Netease_Android</a> © 2025 | GPLv3 LICENSE";
+                "<a href=\"https://github.com/midairlogn/ML-Netease_Android\">ML-Netease_Android</a> © 2026 | GPLv3 LICENSE";
         versionInfo.setText(Html.fromHtml(infoText, Html.FROM_HTML_MODE_LEGACY));
 
         // Init values
